f52618445ec71972c8cfc09dd0f9f5530f756979
Egori11a <114509561+Egori11a@users.noreply.github.com>
2024-03-04 10:31:30 +0300

    Delete OOP_labs/lab01/include directory

--- OOP_labs/lab01/include/func.h
+++
@@ -1,5 +0,0 @@
-#pragma once // говорит компилятору что данный файл должен быть включен только один раз
-
-#include <string>
-
-void function_replace(std::string& text, int n, char old_value, char new_value);
